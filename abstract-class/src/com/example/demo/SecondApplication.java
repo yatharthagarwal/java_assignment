d8e721e734099242b1c2753e5d60b1ca93c81931
Yatharth Agarwal <yatharthagrawal@gmail.com>
2021-01-08 17:29:59 +0530

    Update SecondApplication.java

--- abstract-class/src/com/example/demo/SecondApplication.java
+++ abstract-class/src/com/example/demo/SecondApplication.java
@@ -3,6 +3,5 @@ package com.example.demo;
 public class SecondApplication {
 	public static void main(String[] args) {
 		System.out.println("from yatharth");
-		//System.out.println("From Rashmi");
 	}
 }
